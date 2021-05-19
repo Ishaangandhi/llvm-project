0996c79b7b928af69d98b338e98b4014e7a1ca6d
ishaangandhi <ishaangandhi@gmail.com>
2021-05-19 04:20:51 -0400

    Reformat stuff, add printer for GEP's

--- llvm/lib/Transforms/Interp/Interp.cpp
+++ llvm/lib/Transforms/Interp/Interp.cpp
@@ -20,161 +20,174 @@ using namespace llvm;
 namespace {
 struct Interp : public ModulePass {
 private:
-    std::vector<const GlobalVariable*> globalArrays;
-    std::map<llvm::StringRef, Function*> varToInterpolator;
+  std::vector<const GlobalVariable *> GlobalArrays;
+  std::map<llvm::StringRef, Function *> VarToInterpolator;
 
-    void extractConstNumericArrays(const Module::GlobalListType& globals)
-    {
-        for (auto& global : globals) {
-            errs() << global << " of type " << *global.getType() << ' ';
-            PointerType* PTy = cast<PointerType>(global.getType());
-            if (PTy->getElementType()->isArrayTy() && PTy->getElementType()->getArrayElementType()->isIntegerTy()) {
-                errs() << " is an array." << '\n';
-                globalArrays.push_back(&global);
-            } else {
-                errs() << " is NOT array." << '\n';
-            }
+  void extractConstNumericArrays(const Module::GlobalListType &globals) {
+    for (auto &Global : globals) {
+      errs() << Global << " of type " << *Global.getType() << ' ';
+      PointerType *PTy = cast<PointerType>(Global.getType());
+      if (PTy->getElementType()->isArrayTy() &&
+          PTy->getElementType()->getArrayElementType()->isIntegerTy()) {
+        errs() << " is an array." << '\n';
+        GlobalArrays.push_back(&Global);
+      } else {
+        errs() << " is NOT array." << '\n';
+      }
+    }
+  }
+
+  // template <typename T>
+  std::pair<std::vector<double>, alglib::polynomialfitreport>
+  getInterpolationOf(std::vector<double> ArrayValues) {
+    using namespace alglib;
+
+    // Initialize domain of function
+    std::vector<double> XVec(ArrayValues.size());
+    std::iota(std::begin(XVec), std::end(XVec), 0);
+
+    // Interpolate over all values provided with
+    ae_int_t NumPoints = ArrayValues.size();
+    real_1d_array X;
+    X.setcontent(NumPoints, XVec.data());
+    real_1d_array Y;
+    Y.setcontent(NumPoints, ArrayValues.data());
+    ae_int_t PolynomialDegree = 5;
+    ae_int_t Info;
+    barycentricinterpolant P;
+    polynomialfitreport Rep;
+    std::vector<double> PolynomialCoefficientVec;
+    try {
+      // Try to fit polynomial
+      polynomialfit(X, Y, NumPoints, PolynomialDegree, Info, P, Rep);
+
+      // Convert to barycentric to more usable, polynomial form
+      double Offset = 0.0;
+      double Scale = 1.0;
+      real_1d_array PolynomialCoefficients;
+      polynomialbar2pow(P, Offset, Scale, PolynomialCoefficients);
+      for (int i = 0; i < PolynomialDegree + 1; i++)
+        PolynomialCoefficientVec.push_back(PolynomialCoefficients[i]);
+    } catch (alglib::ap_error &e) {
+      errs() << "Polynomial fit failed: " << e.msg << '\n';
+    }
+    return {PolynomialCoefficientVec, Rep};
+  }
+
+  void
+  insertFunctionFromCoefficients(Module &M,
+                                 std::vector<double> PolynomialCoefficients,
+                                 llvm::StringRef GlobalName) {
+    // Make the function type:  int(int)
+    auto &Ctx = M.getContext();
+    IRBuilder<> builder(Ctx);
+    const auto LLVMint32T = Type::getInt32Ty(Ctx);
+    FunctionType *FT = FunctionType::get(LLVMint32T, {LLVMint32T}, false);
+    const llvm::Twine InterpolatorName = GlobalName + "_interpolator";
+    Function *F =
+        Function::Create(FT, Function::ExternalLinkage, InterpolatorName, M);
+    VarToInterpolator[GlobalName] = F;
+    // Set names for all arguments.
+    for (auto &Arg : F->args())
+      Arg.setName("index");
+
+    // Create a new basic block to start insertion into.
+    BasicBlock *BB = BasicBlock::Create(Ctx, "entry", F);
+    builder.SetInsertPoint(BB);
+
+    // Generate code to compute value at index.
+    // Given a vector of polynomialCoefficients = {c0, c1, c2, c3, c4, c5} and a
+    // vector of values xs = {1, x, x^2, x^3, x^4, x^5}, take dot product into
+    // ds = {d0, d1, d2, d3, d4, d5}, and perform sum (d0...d5).
+    std::vector<llvm::Value *> xs = {ConstantInt::get(LLVMint32T, 1)};
+    Argument *x = F->getArg(0); // Only argument of function is the index
+    for (size_t I = 1; I < 6; I++)
+      xs.push_back(builder.CreateMul(xs.back(), x, "x" + std::to_string(I)));
+    std::vector<llvm::Value *> Ds;
+    for (size_t I = 0; I < 6; I++) {
+      auto c_i =
+          ConstantInt::get(LLVMint32T, (int)(PolynomialCoefficients[I] + 0.5));
+      auto x_i = xs[I];
+      Ds.push_back(builder.CreateMul(c_i, x_i, "d" + std::to_string(I)));
+    }
+    std::vector<llvm::Value *> Sums = {Ds[0]};
+    for (size_t i = 1; i < 6; i++)
+      Sums.push_back(
+          builder.CreateAdd(Sums.back(), Ds[i], "sum" + std::to_string(i)));
+    builder.CreateRet(Sums.back());
+  }
+
+  template <typename T> void printVec(std::vector<T> vec) {
+    errs() << "[ ";
+    for (auto e : vec)
+      errs() << e << " ";
+    errs() << "]\n";
+  }
+
+  void insertInterpolationFunctions(Module &M) {
+    for (const auto *array : GlobalArrays) {
+      auto arraySize =
+          array->getType()->getElementType()->getArrayNumElements();
+      std::vector<double> arrayValues;
+      for (size_t i = 0; i < arraySize; i++) {
+        auto intVal = dyn_cast<ConstantInt>(
+                          array->getInitializer()->getAggregateElement(i))
+                          ->getZExtValue();
+        arrayValues.push_back(intVal);
+      }
+      printVec(arrayValues);
+      auto [polynomialCoefficients, fitreport] =
+          getInterpolationOf(arrayValues);
+      printVec(polynomialCoefficients);
+      insertFunctionFromCoefficients(M, polynomialCoefficients,
+                                     array->getName());
+    }
+  }
+
+  void replaceArrayAccesses(Module &M) {
+    for (auto &f : M.getFunctionList()) {
+      for (auto &bb : f.getBasicBlockList()) {
+        errs() << "\nBasic block";
+        for (auto &ins : bb) {
+          if (ins.getOpcode() == Instruction::GetElementPtr) {
+            auto &gep = dyn_cast<GEPOperator>(ins);
+            errs() << "--Got a gep:\n";
+            errs() << '\t';
+            ins.print(errs());
+            errs() << '\n';
+          }
         }
+      }
     }
-
-    // template <typename T>
-    std::pair<std::vector<double>, alglib::polynomialfitreport>
-    getInterpolationOf(std::vector<double> arrayValues)
-    {
-        using namespace alglib;
-
-        // Initialize domain of function
-        std::vector<double> xVec(arrayValues.size());
-        std::iota(std::begin(xVec), std::end(xVec), 0);
-
-        // Interpolate over all values provided with
-        ae_int_t numPoints = arrayValues.size();
-        real_1d_array x;
-        x.setcontent(numPoints, xVec.data());
-        real_1d_array y;
-        y.setcontent(numPoints, arrayValues.data());
-        ae_int_t polynomialDegree = 5;
-        ae_int_t info;
-        barycentricinterpolant p;
-        polynomialfitreport rep;
-        std::vector<double> polynomialCoefficientVec;
-        try {
-            // Try to fit polynomial
-            polynomialfit(x, y, numPoints, polynomialDegree, info, p, rep);
-
-            // Convert to barycentric to more usable, polynomial form
-            double offset = 0.0;
-            double scale = 1.0;
-            real_1d_array polynomialCoefficients;
-            polynomialbar2pow(p, offset, scale, polynomialCoefficients);
-            for (int i = 0; i < polynomialDegree + 1; i++)
-                polynomialCoefficientVec.push_back(polynomialCoefficients[i]);
-        } catch (alglib::ap_error e) {
-            errs() << "Polynomial fit failed: " << e.msg << '\n';
-        }
-        return { polynomialCoefficientVec, rep };
-    }
-
-    void insertFunctionFromCoefficients(
-        Module& M,
-        std::vector<double> polynomialCoefficients,
-        llvm::StringRef globalName)
-    {
-        // Make the function type:  int(int)
-        auto& ctx = M.getContext();
-        IRBuilder<> builder(ctx);
-        const auto llvmint32_t = Type::getInt32Ty(ctx);
-        FunctionType* FT = FunctionType::get(llvmint32_t, { llvmint32_t }, false);
-        const llvm::Twine interpolatorName = globalName + "_interpolator";
-        Function* F = Function::Create(FT, Function::ExternalLinkage, interpolatorName, M);
-        varToInterpolator[globalName] = F;
-        // Set names for all arguments.
-        for (auto& Arg : F->args())
-            Arg.setName("index");
-
-        // Create a new basic block to start insertion into.
-        BasicBlock* BB = BasicBlock::Create(ctx, "entry", F);
-        builder.SetInsertPoint(BB);
-
-        // Generate code to compute value at index.
-        // Given a vector of polynomialCoefficients = {c0, c1, c2, c3, c4, c5} and a
-        // vector of values xs = {1, x, x^2, x^3, x^4, x^5}, take dot product into
-        // ds = {d0, d1, d2, d3, d4, d5}, and perform sum (d0...d5).
-        std::vector<llvm::Value*> xs = { ConstantInt::get(llvmint32_t, 1) };
-        Argument* x = F->getArg(0); // Only argument of function is the index
-        for (size_t i = 1; i < 6; i++)
-            xs.push_back(builder.CreateMul(xs.back(), x, "x" + std::to_string(i)));
-        std::vector<llvm::Value*> ds;
-        for (size_t i = 0; i < 6; i++) {
-            auto c_i = ConstantInt::get(llvmint32_t, (int)(polynomialCoefficients[i] + 0.5));
-            auto x_i = xs[i];
-            ds.push_back(builder.CreateMul(c_i, x_i, "d" + std::to_string(i)));
-        }
-        std::vector<llvm::Value*> sums = { ds[0] };
-        for (size_t i = 1; i < 6; i++)
-            sums.push_back(
-                builder.CreateAdd(sums.back(), ds[i], "sum" + std::to_string(i)));
-        builder.CreateRet(sums.back());
-    }
-
-    template <typename T>
-    void printVec(std::vector<T> vec)
-    {
-        errs() << "[ ";
-        for (auto e : vec)
-            errs() << e << " ";
-        errs() << "]\n";
-    }
-
-    void insertInterpolationFunctions(Module& M)
-    {
-        for (auto array : globalArrays) {
-            auto arraySize = array->getType()->getElementType()->getArrayNumElements();
-            std::vector<double> arrayValues;
-            for (size_t i = 0; i < arraySize; i++) {
-                auto intVal = dyn_cast<ConstantInt>(array->getInitializer()->getAggregateElement(i))
-                                  ->getZExtValue();
-                arrayValues.push_back(intVal);
-            }
-            printVec(arrayValues);
-            auto [polynomialCoefficients, fitreport] = getInterpolationOf(arrayValues);
-            printVec(polynomialCoefficients);
-            insertFunctionFromCoefficients(
-                M, polynomialCoefficients, array->getName());
-        }
-    }
+  }
 
 public:
-    static char ID;
-    Interp()
-        : ModulePass(ID)
-    {
-    }
+  static char ID;
+  Interp() : ModulePass(ID) {}
 
-    bool runOnModule(Module& M) override
-    {
-        // Extract all global constant numeric arrays
-        extractConstNumericArrays(M.getGlobalList());
+  bool runOnModule(Module &M) override {
+    // Extract all global constant numeric arrays
+    extractConstNumericArrays(M.getGlobalList());
 
-        // Perform polynomial interpolation and create a function
-        // for each global constant array
-        insertInterpolationFunctions(M);
+    // Perform polynomial interpolation and create a function
+    // for each global constant array
+    insertInterpolationFunctions(M);
 
-        // Replace references to the array with accesses into the function
+    // Replace references to the array with accesses into the function
+    replaceArrayAccesses(M);
 
-        return false;
-    }
+    return false;
+  }
 }; // end of struct Interp
 } // end of anonymous namespace
 
 char Interp::ID = 0;
-static RegisterPass<Interp> X("Interp",
-    "Polynomial Interpolation Pass",
-    false /* Only looks at CFG */,
-    false /* Analysis Pass */);
+static RegisterPass<Interp> X("Interp", "Polynomial Interpolation Pass",
+                              false /* Only looks at CFG */,
+                              false /* Analysis Pass */);
 
 static RegisterStandardPasses Y(PassManagerBuilder::EP_EarlyAsPossible,
-    [](const PassManagerBuilder& Builder,
-        legacy::PassManagerBase& PM) {
-        PM.add(new Interp());
-    });
+                                [](const PassManagerBuilder &Builder,
+                                   legacy::PassManagerBase &PM) {
+                                  PM.add(new Interp());
+                                });
--- llvm/lib/Transforms/Interp/hello.ll
+++ llvm/lib/Transforms/Interp/hello.ll
@@ -1,14 +1,13 @@
 ; ModuleID = 'hello.c'
 source_filename = "hello.c"
-target datalayout = "e-m:o-i64:64-f80:128-n8:16:32:64-S128"
-target triple = "x86_64-apple-macosx10.15.0"
+target datalayout = "e-m:o-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-apple-macosx11.0.0"
 
 @values = local_unnamed_addr constant [6 x i32] [i32 0, i32 1, i32 8, i32 27, i32 64, i32 125], align 16
 @array2 = local_unnamed_addr constant [5 x i32] [i32 27, i32 64, i32 125, i32 216, i32 343], align 16
 @const_float = local_unnamed_addr global float 0x40091EB860000000, align 4
-
 ; Function Attrs: norecurse nounwind readnone ssp uwtable
-define i32 @foo(i32) local_unnamed_addr #0 {
+define i32 @foo(i32 %0) local_unnamed_addr #0 {
   %2 = zext i32 %0 to i64
   %3 = getelementptr inbounds [6 x i32], [6 x i32]* @values, i64 0, i64 %2
   %4 = load i32, i32* %3, align 4, !tbaa !4
@@ -19,16 +18,28 @@ define i32 @foo(i32) local_unnamed_addr #0 {
   %9 = mul nsw i32 %8, %4
   ret i32 %9
 }
+; Function Attrs: norecurse nounwind readnone ssp uwtable
+define i32 @main(i32 %0, i8** nocapture readnone %1) local_unnamed_addr #0 {
+  %3 = zext i32 %0 to i64
+  %4 = getelementptr inbounds [6 x i32], [6 x i32]* @values, i64 0, i64 %3
+  %5 = load i32, i32* %4, align 4, !tbaa !4
+  %6 = add i32 %0, -1
+  %7 = zext i32 %6 to i64
+  %8 = getelementptr inbounds [6 x i32], [6 x i32]* @values, i64 0, i64 %7
+  %9 = load i32, i32* %8, align 4, !tbaa !4
+  %10 = mul nsw i32 %9, %5
+  ret i32 %10
+}
 
 attributes #0 = { norecurse nounwind readnone ssp uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "darwin-stkchk-strong-link" "disable-tail-calls"="false" "frame-pointer"="all" "less-precise-fpmad"="false" "min-legal-vector-width"="0" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "probe-stack"="___chkstk_darwin" "stack-protector-buffer-size"="8" "target-cpu"="penryn" "target-features"="+cx16,+cx8,+fxsr,+mmx,+sahf,+sse,+sse2,+sse3,+sse4.1,+ssse3,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
 
 !llvm.module.flags = !{!0, !1, !2}
 !llvm.ident = !{!3}
 
-!0 = !{i32 2, !"SDK Version", [3 x i32] [i32 10, i32 15, i32 6]}
+!0 = !{i32 2, !"SDK Version", [2 x i32] [i32 11, i32 1]}
 !1 = !{i32 1, !"wchar_size", i32 4}
 !2 = !{i32 7, !"PIC Level", i32 2}
-!3 = !{!"Apple clang version 11.0.3 (clang-1103.0.32.62)"}
+!3 = !{!"Apple clang version 12.0.0 (clang-1200.0.32.29)"}
 !4 = !{!5, !5, i64 0}
 !5 = !{!"int", !6, i64 0}
 !6 = !{!"omnipotent char", !7, i64 0}
